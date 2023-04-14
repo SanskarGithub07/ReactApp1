56c238423644a86977e69b9410ea516399dd1c14
SanskarGithub07 <3899.sanskarsugandhi@gmail.com>
2023-04-15 03:05:39 +0530

    About Component

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,26 +1,32 @@
 import React from 'react'
 import logo from '../Assets/logo_website.png'
+import hamburger_website from '../Assets/hamburger_website.png'
 import '../Styles/Navbar.css'
 
 function Navbar() {
-  return (
-    <div className = 'navbar_main_container'>
-        <div className = 'logo_container'>
-            <img src = {logo} />       
+    const [showNav, setShowNav] = React.useState(false)
+    return (
+        <div className='navbar_main_container'>
+            <div className='phone_container'>
+                <div className='logo_container'>
+                    <img src={logo} />
+                </div>
+                <img onClick={() => { setShowNav(!showNav) }} className='hamburger' src={hamburger_website} />
+
+            </div>
+            <div className={'list_signup ' + (showNav && 'active')}>
+                <div className='list_container'>
+                    <p>Product</p>
+                    <p>Resource</p>
+                    <p>Pricing</p>
+                    <p>Explore</p>
+                </div>
+                <div className='signup_container'>
+                    <p>Sign Up</p>
+                </div>
+            </div>
         </div>
-        <div className = 'list_signup'>
-        <div className = 'list_container'>
-            <p>Product</p>
-            <p>Resource</p>
-            <p>Pricing</p>
-            <p>Explore</p>
-        </div>
-        </div>
-        <div className = 'signup_container'>
-                <p>Sign Up</p>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar
